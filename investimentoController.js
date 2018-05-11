b959fe2f1ea8cb686676af0e7835407d843c2c85
GeorgemCabral <georgemcabral@live.com>
2018-05-11 15:29:48 -0300

    Commit
    graphics

--- investimentoController.js
+++ investimentoController.js
@@ -1,4 +1,4 @@
-angular.module('primeiraApp').controller('investimentoCtrl', [
+angular.module('Wallet').controller('investimentoCtrl', [
   '$scope',
   investimentoController
 ])
@@ -12,4 +12,31 @@ function investimentoController($scope){
     $scope.valorSelic= $scope.valor * ( Math.pow(1.00462 , $scope.meses)) ;
     $scope.valorCdb = $scope.valor * ( Math.pow(1.008176 , $scope.meses)) ;
   };
+
+  
+}
+
+google.load('vizualization','1.0', {'packages':['corechat']});
+
+google.setOnLoadCallback(demonstrar);
+
+function demonstrar()
+{
+  var data = new google.vizualization.Datatable();
+  data.addColumn('string', 'Credito');
+  data.addColumn('string', 'Debito');
+  data.addRowls(
+  [
+   ['aluguel', 1500],
+   ['comida', 1000],
+
+  ]
+  );
+var options = {'title': 'Resumo',
+              'width': 500,
+              'height': 300};
+
+var graphic = new google.vizualization.PieChart(document.getElementById('charts'));
+graphic.draw(data, options);              
+  
 }
