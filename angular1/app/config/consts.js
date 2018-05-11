eda3de76274f3f64133cf4a1f18df5e182076359
Victor Conde <victorconde91@gmail.com>
2018-05-11 17:47:08 -0300

    Alteração front

--- angular1/app/config/consts.js
+++ angular1/app/config/consts.js
@@ -1,9 +1,9 @@
 angular.module('primeiraApp').constant('consts', {
-  appName: 'MEAN - Primeira Aplicação',
+  appName: 'WalletApp',
   version: '1.0',
-  owner: 'Cod3r',
-  year: '2017',
-  site: 'http://cod3r.com.br',
+  owner: '',
+  year: '2018',
+  site: '',
   apiUrl: 'http://localhost:3003/api',
 }).run(['$rootScope', 'consts', function($rootScope, consts) {
   $rootScope.consts = consts
