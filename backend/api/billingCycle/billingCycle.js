c7fc0cfca57f9242d514c588861efb1b51e3f6e7
Victor Conde <victorcondeb@gmail.com>
2018-06-15 17:02:57 -0300

    Spring 7.2

--- backend/api/billingCycle/billingCycle.js
+++ backend/api/billingCycle/billingCycle.js
@@ -9,12 +9,14 @@ const creditSchema = new mongoose.Schema({
 const debtSchema = new mongoose.Schema({
   name: { type: String, required: true },
   value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
+  category: { type: String, required: false, uppercase:true,
+    enum: ['LAZER', 'INVESTIMENTO', 'ALIMENTAÇÃO', 'MORADIA'] },
   status: { type: String, required: false, uppercase: true,
     enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
 const billingCycleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: false },
   month: { type: Number, min: 1, max: 12, required: true },
   year: { type: Number, min: 1970, max: 2100, required: true },
   credits: [creditSchema],
